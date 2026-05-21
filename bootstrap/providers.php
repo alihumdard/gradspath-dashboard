2129ce54927f7d05cb276571cd799818cefd928a
Abdul Rauf <raufie384@gmail.com>
2026-05-21 11:59:48 +0500

    feat: add Laravel Telescope integration with configuration and migration

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,6 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\HorizonServiceProvider::class,
+    App\Providers\TelescopeServiceProvider::class,
     Modules\Auth\app\Providers\AuthServiceProvider::class,
     Modules\Discovery\app\Providers\DiscoveryServiceProvider::class,
 ];
